1dc59f5d4158c32350b7d1cb19823190ac34b392
thaiquminh <thai.qu.minh@gmail.com>
2023-09-25 12:44:03 +0700

    fix cache with mutex

--- ch9/c9_7/cache.go
+++ ch9/c9_7/cache.go
@@ -1,11 +1,26 @@
 package c9_7
 
-import "sync"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
 
 type Memo struct {
 	f     Func
 	cache map[string]*entry
 	mu    sync.Mutex
+}
+
+func NewMemo(f Func) *Memo {
+	return &Memo{
+		f,
+		make(map[string]*entry),
+		sync.Mutex{},
+	}
 }
 
 type Func func(key string) (any, error)
@@ -34,4 +49,37 @@ func (memo *Memo) Get(key string) (any, error) {
 		<-e.ready
 	}
 	return e.res.value, e.res.err
+}
+
+func httpGet(url string) (any, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func Run() {
+	memo := NewMemo(httpGet)
+	urls := []string{
+		"https://golang.org",
+		"https://godoc.org",
+		"https://play.golang.org",
+		"http://gopl.io",
+		"https://golang.org",
+		"https://godoc.org",
+		"https://play.golang.org",
+		"http://gopl.io",
+	}
+	for _, url := range urls {
+		start := time.Now()
+		res, err := memo.Get(url)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("%s %s %d bytes\n", url, time.Since(start).String(), len(res.([]byte)))
+	}
 }
--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/t0nem1n/goplexercise/ch9/c9_7_2"
+	"github.com/t0nem1n/goplexercise/ch9/c9_7"
 )
 
 func main() {
 	start := time.Now()
-	c9_7_2.Run()
+	c9_7.Run()
 	fmt.Printf("running in %v\n", time.Since(start).String())
 }
